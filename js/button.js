747b734b4fc8ea53216562fbe7e3b82fce065b31
Banaanae <83927639+Banaanae@users.noreply.github.com>
2025-03-17 16:06:39 +1100

    add profile importing

--- js/button.js
+++ js/button.js
@@ -6,40 +6,47 @@ function parseProfile(path) {
     fetch(path)
         .then(response => response.text())
         .then(fileContents => {
-            const profile = JSON.parse(fileContents)
-            console.log(profile)
-            console.log(profile.chars)
-
-            for (const [category, value] of Object.entries(profile.chars)) {
-                console.log(category, value);
-
-                let div = document.createElement('div')
-                let input = document.createElement('input')
-                let h2 = document.createElement('h2')
-                let label = document.createElement('label')
-
-                input.type = 'checkbox'
-                input.checked = value.checked
-                
-                label.innerHTML = '&nbsp;' + value.name
-                
-                h2.appendChild(input)
-                h2.appendChild(label)
-                
-                div.appendChild(h2)
-
-                document.querySelector('.buttons').appendChild(div)
-
-                value.buttons.forEach(function(character) {
-                    createButton(character, div);
-                });
-            };
+            createProfile(fileContents)
+        })
+        .catch(e => {
+            createProfile(localStorage[path])
         })
 
-    function createButton(content, div) {
-        const button = document.createElement('button');
-        button.innerHTML = content;
-        div.appendChild(button);
+    function createProfile(fileContents) {
+        const profile = JSON.parse(fileContents)
+        console.log(profile)
+        console.log(profile.chars)
+
+        for (const [category, value] of Object.entries(profile.chars)) {
+            console.log(category, value);
+
+            let div = document.createElement('div')
+            let input = document.createElement('input')
+            let h2 = document.createElement('h2')
+            let label = document.createElement('label')
+
+            input.type = 'checkbox'
+            input.checked = value.checked
+            
+            label.innerHTML = '&nbsp;' + value.name
+            
+            h2.appendChild(input)
+            h2.appendChild(label)
+            
+            div.appendChild(h2)
+
+            document.querySelector('.buttons').appendChild(div)
+
+            value.buttons.forEach(function(character) {
+                createButton(character, div);
+            });
+        };
+
+        function createButton(content, div) {
+            const button = document.createElement('button');
+            button.innerHTML = content;
+            div.appendChild(button);
+        }
     }
 }
 
--- js/profiles.js
+++ js/profiles.js
@@ -11,23 +11,72 @@ createProfileOption('../chars/profiles/default.json')
 createProfileOption('../chars/profiles/full.json')
 createProfileOption('../chars/profiles/unicode.json')
 
+// Add custom profiles from local storage
+for (const [profile] of Object.entries(localStorage)) {
+    createProfileOption(profile)
+}
+
 function createProfileOption(profile) {
-    let prflLink = document.createElement('a')
-    let prflName;
     fetch(profile)
         .then(response => response.text())
         .then(fileContents => {
-            prflName = JSON.parse(fileContents)
-            prflLink.innerText = prflName.name
-            prflLink.href = '#'
-            prflLink.id = profile.replace(/.+\/(\w+)\.json/, '$1')
+            createOption(fileContents)
+        })
+        .catch(() => { // else local storage
+            createOption(localStorage[profile], profile)
+        })
 
+    function createOption(fileContents, localName = '') {
+        let prflLink = document.createElement('a')
+
+        prflName = JSON.parse(fileContents)
+        prflLink.innerText = prflName.name
+        prflLink.href = '#'
+        if (localName === '') {
+            prflLink.id = profile.replace(/.+\/(\w+)\.json/, '$1')
+        } else {
+            prflLink.id = localName
+        }
+
+        // TODO: Not hard coded
+        if (document.querySelector('.profileList').childElementCount < 3) {
             prflLink.addEventListener('click', function (elem) {
                 console.log(elem)
-                document.querySelector('.profileBtn').innerText = prflName.name
+                document.querySelector('.profileBtn').innerText = elem.target.innerText
                 switchProfile('../chars/profiles/' + elem.target.id + '.json')
             })
-        
-            document.querySelector('.profileList').appendChild(prflLink)
-        })
+        } else {
+            prflLink.addEventListener('click', function (elem) {
+                document.querySelector('.profileBtn').innerText = elem.target.innerText
+                switchProfile(elem.target.id)
+            })
+        }
+    
+        document.querySelector('.profileList').appendChild(prflLink)
+    }
 }
+
+let importPrfl = document.getElementById('import')
+
+importPrfl.addEventListener('change', function() {
+    let files = importPrfl.files
+
+    if (files.length > 0) {
+        for (let i = 0; i < files.length; i++) {
+            let reader = new FileReader();
+            let file = files[i];
+            reader.onload = function(e) {
+                let content = e.target.result
+                let name = file.name.replace(/(.+)\.json/, '$1')
+                let i = 1
+                while (localStorage.name) {
+                    name = file.name.replace(/(.+)\.json/, '$1') + i
+                    i++
+                }
+                localStorage.setItem(name, content)
+                createProfileOption(name)
+            }
+            reader.readAsText(file)
+        }
+    }
+})
