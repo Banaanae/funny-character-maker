61956f25d740a03060d8e3e254a9af7545f73d5f
Banaanae <83927639+Banaanae@users.noreply.github.com>
2025-07-31 10:57:18 +1000

    undo and redo

--- js/editor.js
+++ js/editor.js
@@ -35,6 +35,10 @@ document.getElementById("editor2Manage").addEventListener("click", function() {
 })
 
 function editText(event) {
+    if (changeLog.usingRevision + 1 !== changeLog.changes.length)
+        changeLog.changes.splice(changeLog.usingRevision + 1)
+
+    //event.preventDefault()
     let curText = event.target.innerText
     event.target.innerText = ""
 
@@ -42,10 +46,10 @@ function editText(event) {
     input.value = curText
     event.target.appendChild(input)
 
-    input.addEventListener("focusout", saveEditText)
-    input.addEventListener("keyup", saveEditText)
+    input.addEventListener("focusout", saveEditText.bind(null, curText))
+    input.addEventListener("keyup", saveEditText.bind(null, curText))
 
-    function saveEditText(event2) {
+    function saveEditText(curText, event2) {
         if (event2.type === "keyup" && event2.key !== "Enter")
             return
 
@@ -55,14 +59,17 @@ function editText(event) {
         parent.innerHTML = ""
         parent.innerText = newText
         
-        changeLog.changes.push({ type: "renamed", cat: parent.parentElement.getAttribute("name"), button: parent.getAttribute("index"), newtext: newText })
+        changeLog.changes.push({ type: "renamed", cat: parent.parentElement.getAttribute("name"), button: parent.getAttribute("index"), oldtext: curText, newtext: newText })
         changeLog.usingRevision++
         changesAfterSave = true
     }
 }
 
 function removeButton(event) {
-    changeLog.changes.push({ type: "removed", cat: event.target.parentElement.getAttribute("name"), button: event.target.getAttribute("index") })
+    if (changeLog.usingRevision + 1 !== changeLog.changes.length)
+        changeLog.changes.splice(changeLog.usingRevision + 1)
+
+    changeLog.changes.push({ type: "removed", cat: event.target.parentElement.getAttribute("name"), button: event.target.getAttribute("index"), text: event.target.innerText })
     event.target.remove()
 
     changeLog.usingRevision++
@@ -84,7 +91,6 @@ function applyChanges() {
         })
     
     function applyAndFinalise(dataStr) {
-        console.log(dataStr)
         for (let i = 0; changeLog.usingRevision >= i; i++) {
             let change = changeLog.changes[i]
 
@@ -97,6 +103,61 @@ function applyChanges() {
         localStorage.test = JSON.stringify(dataStr)
         alert("Saved profile to [name]" )
     }
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === 'z') {
+        event.preventDefault()
+        undo()
+    } else if (event.ctrlKey && event.key === 'y') {
+        event.preventDefault()
+        redo()
+    }
+})
+
+function undo() {
+    if (changeLog.usingRevision === -1)
+        return
+    
+    let toUndo = changeLog.changes[changeLog.usingRevision]
+    let button
+    if (toUndo.type === "removed") {
+        button = document.createElement("button")
+        button.innerText = toUndo.text
+        button.setAttribute("index", toUndo.button)
+        if (toUndo.button > document.querySelector(`div[name="${toUndo.cat}"] > button:last-child`).getAttribute("index")) {
+            document.querySelector(`div[name="${toUndo.cat}"]`).appendChild(button)
+        } else {
+            let i = 1
+            while (toUndo.button < document.querySelector(`div[name="${toUndo.cat}"] > button:nth-last-child(${i})`).getAttribute("index")) {
+                i = i + 1
+            }
+            document.querySelector(`div[name="${toUndo.cat}"]`).insertBefore(button, document.querySelector(`div[name="${toUndo.cat}"] > button:nth-last-child(${--i})`))
+        }
+    } else if (toUndo.type === "renamed") {
+        button = document.querySelector(`div[name="${toUndo.cat}"] > button[index="${toUndo.button}"]`)
+        button.innerText = toUndo.oldtext
+    }
+    button.addEventListener('dblclick', editText)
+    button.addEventListener('contextmenu', removeButton)
+    changeLog.usingRevision--
+}
+
+function redo() {
+    if (changeLog.usingRevision === changeLog.changes.length)
+        return
+    
+    changeLog.usingRevision++
+    let toRedo = changeLog.changes[changeLog.usingRevision]
+
+    let event = {}
+    event.parent = document.querySelector(`div[name="${toRedo.cat}"]`)
+    event.target = event.parent.querySelector(`button[index="${toRedo.button}"]`)
+
+    if (toRedo.type === "removed")
+        removeButton(event)
+    else if (toRedo.type === "renamed")
+        editText(event)
 }
 
 function getAvailableName() {
